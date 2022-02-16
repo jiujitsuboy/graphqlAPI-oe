abbaed0e1ef11358d928dbf96ccfbb3584dcfd25
jose nino <josealejonino@gmail.com>
2022-02-16 00:09:41 -0500

    remove commented and demo classes

--- oe-hr-portal-service-webapp/src/main/java/com/openenglish/pp/controller/JwtTokenController.java
+++ oe-hr-portal-service-webapp/src/main/java/com/openenglish/pp/controller/JwtTokenController.java
@@ -15,37 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-//@RestController
-//@RequestMapping(value = "sso-tokens", produces = APPLICATION_JSON_VALUE)
+@RestController
+@RequestMapping(value = "sso-tokens", produces = APPLICATION_JSON_VALUE)
 public class JwtTokenController {
 
-//  private final static Logger logger = LoggerFactory.getLogger(JwtTokenController.class);
-//  private JwtTokenService jwtTokenService;
-//
-//  @Autowired
-//  public JwtTokenController(JwtTokenService jwtTokenService) {
-//    this.jwtTokenService = jwtTokenService;
-//  }
-//
-//
-//
-//  /**
-//   * Returning the decoded token
-//   *
-//   * @param accessToken
-//   * @return
-//   * status: 200 if the request gets processed and the  result payload is returned
-//   * status: 400 if accessToken is missing
-//   * status: 500 if there's an error
-//   */
-//  @RequestMapping(method = {RequestMethod.GET})
-//  public ResponseEntity getDecodedToken(@RequestParam(value = "accessToken",required = true) String accessToken) {
-//    try {
-//      TokenDecodedInfo result = this.jwtTokenService.getTokenDecodedInfo(accessToken);
-//      return new ResponseEntity(result, OK);
-//    } catch (Exception ex) {
-//      logger.error("Failed getting decoded token for: " + accessToken, ex);
-//      throw ex;
-//    }
-//  }
+  private final static Logger logger = LoggerFactory.getLogger(JwtTokenController.class);
+  private JwtTokenService jwtTokenService;
+
+  @Autowired
+  public JwtTokenController(JwtTokenService jwtTokenService) {
+    this.jwtTokenService = jwtTokenService;
+  }
+
+
+
+  /**
+   * Returning the decoded token
+   *
+   * @param accessToken
+   * @return
+   * status: 200 if the request gets processed and the  result payload is returned
+   * status: 400 if accessToken is missing
+   * status: 500 if there's an error
+   */
+  @RequestMapping(method = {RequestMethod.GET})
+  public ResponseEntity getDecodedToken(@RequestParam(value = "accessToken",required = true) String accessToken) {
+    try {
+      TokenDecodedInfo result = this.jwtTokenService.getTokenDecodedInfo(accessToken);
+      return new ResponseEntity(result, OK);
+    } catch (Exception ex) {
+      logger.error("Failed getting decoded token for: " + accessToken, ex);
+      throw ex;
+    }
+  }
 }
--- oe-hr-portal-service-webapp/src/main/java/com/openenglish/pp/controller/MyGlobalExceptionHandler.java
+++ oe-hr-portal-service-webapp/src/main/java/com/openenglish/pp/controller/MyGlobalExceptionHandler.java
@@ -1,55 +1,55 @@
 package com.openenglish.pp.controller;
-//
-//
-//import com.openenglish.pp.common.api.model.ErrorResponse;
-//
-//import org.apache.commons.lang.StringUtils;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.MissingServletRequestParameterException;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.ws.rs.BadRequestException;
 
-//@ControllerAdvice
+
+import com.openenglish.pp.common.api.model.ErrorResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
+
+@ControllerAdvice
 public  class MyGlobalExceptionHandler {
-//  private static final Logger logger = LoggerFactory.getLogger(MyGlobalExceptionHandler.class);
-//
-//  @ExceptionHandler({BadRequestException.class, IllegalArgumentException.class,
-//                     MissingServletRequestParameterException.class})
-//  public ResponseEntity handleBadRequest(HttpServletRequest request, Exception e) {
-//    logger.error("Bad request ", e);
-//    return new ResponseEntity(new ErrorResponse().setErrorCode(
-//        HttpStatus.BAD_REQUEST.value()).setErrorMessage(e.getMessage())
-//        , HttpStatus.BAD_REQUEST);
-//  }
-//
-//  @ExceptionHandler({ResourceNotFoundException.class})
-//  public ResponseEntity handleResourceNotFound(HttpServletRequest request, Exception e) {
-//    return new ResponseEntity(new ErrorResponse().setErrorCode(HttpStatus.NOT_FOUND.value()).setErrorMessage(e.getMessage()), HttpStatus.NOT_FOUND);
-//  }
-//
-//  @ExceptionHandler(Exception.class)
-//  public ResponseEntity handle(HttpServletRequest request, Exception e) {
-//    logger.error("Failed to handle request for url: "+getFullURL(request), e);
-//    return new ResponseEntity(new ErrorResponse().setErrorCode(
-//        HttpStatus.INTERNAL_SERVER_ERROR.value()).setErrorMessage(e.getMessage())
-//        , HttpStatus.INTERNAL_SERVER_ERROR);
-//  }
-//
-//  public String getFullURL(HttpServletRequest request) {
-//    StringBuffer requestURL = request.getRequestURL();
-//    String queryString = request.getQueryString();
-//
-//    if (StringUtils.isBlank(queryString)) {
-//      return requestURL.toString();
-//    } else {
-//      return requestURL.append('?').append(queryString).toString();
-//    }
-//  }
+  private static final Logger logger = LoggerFactory.getLogger(MyGlobalExceptionHandler.class);
+
+  @ExceptionHandler({BadRequestException.class, IllegalArgumentException.class,
+                     MissingServletRequestParameterException.class})
+  public ResponseEntity handleBadRequest(HttpServletRequest request, Exception e) {
+    logger.error("Bad request ", e);
+    return new ResponseEntity(new ErrorResponse().setErrorCode(
+        HttpStatus.BAD_REQUEST.value()).setErrorMessage(e.getMessage())
+        , HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({ResourceNotFoundException.class})
+  public ResponseEntity handleResourceNotFound(HttpServletRequest request, Exception e) {
+    return new ResponseEntity(new ErrorResponse().setErrorCode(HttpStatus.NOT_FOUND.value()).setErrorMessage(e.getMessage()), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity handle(HttpServletRequest request, Exception e) {
+    logger.error("Failed to handle request for url: "+getFullURL(request), e);
+    return new ResponseEntity(new ErrorResponse().setErrorCode(
+        HttpStatus.INTERNAL_SERVER_ERROR.value()).setErrorMessage(e.getMessage())
+        , HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  public String getFullURL(HttpServletRequest request) {
+    StringBuffer requestURL = request.getRequestURL();
+    String queryString = request.getQueryString();
+
+    if (StringUtils.isBlank(queryString)) {
+      return requestURL.toString();
+    } else {
+      return requestURL.append('?').append(queryString).toString();
+    }
+  }
 }
