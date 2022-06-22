647dadb8f7db7e33f3a4c2f6ca93db1f3b288d72
Marius Cana <mariuscana@gmail.com>
2022-06-22 12:52:56 +0300

    HRPV2-187 reformat with Google code style ( new files )

--- oe-hr-portal-service-webapp/src/main/java/com/openenglish/hr/APIGatewayCompatibleBearerTokenResolver.java
+++ oe-hr-portal-service-webapp/src/main/java/com/openenglish/hr/APIGatewayCompatibleBearerTokenResolver.java
@@ -1,9 +1,5 @@
 package com.openenglish.hr;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,9 +9,13 @@ import org.springframework.security.oauth2.server.resource.web.BearerTokenResolv
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.util.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
- * Modified copy of {@link DefaultBearerTokenResolver} to
- * match Authorization headers having the  format :
+ * Modified copy of {@link DefaultBearerTokenResolver} to match Authorization headers having the  format :
  * <pre> Authorization: access_token</pre>
  * instead of
  * <pre> Authorization: Bearer access_token</pre>
@@ -29,101 +29,102 @@ import org.springframework.util.StringUtils;
  * <p>The default {@link BearerTokenResolver} implementation based on RFC 6750.</p>
  *
  * @author Vedran Pavic
+ * @see <a href="https://tools.ietf.org/html/rfc6750#section-2" target="_blank">RFC 6750 Section 2: Authenticated
+ * Requests</a>
  * @since 5.1
- * @see <a href="https://tools.ietf.org/html/rfc6750#section-2" target="_blank">RFC 6750 Section 2: Authenticated Requests</a>
  */
 public final class APIGatewayCompatibleBearerTokenResolver implements BearerTokenResolver {
 
-	// APIGatewayCompatibleBearerTokenResolver divergence from DefaultBearerTokenResolver
-	// source class was using pattern : "^Bearer (?<token>[a-zA-Z0-9-._~+/]+)=*$"
-	private static final Pattern authorizationPattern = Pattern.compile("^(?<token>[a-zA-Z0-9-._~+/]+)=*$");
+  // APIGatewayCompatibleBearerTokenResolver divergence from DefaultBearerTokenResolver
+  // source class was using pattern : "^Bearer (?<token>[a-zA-Z0-9-._~+/]+)=*$"
+  private static final Pattern authorizationPattern = Pattern.compile("^(?<token>[a-zA-Z0-9-._~+/]+)=*$");
 
-	private boolean allowFormEncodedBodyParameter = false;
+  private boolean allowFormEncodedBodyParameter = false;
 
-	private boolean allowUriQueryParameter = false;
+  private boolean allowUriQueryParameter = false;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String resolve(HttpServletRequest request) {
-		String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
-		String parameterToken = resolveFromRequestParameters(request);
-		if (authorizationHeaderToken != null) {
-			if (parameterToken != null) {
-				BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
-						HttpStatus.BAD_REQUEST,
-						"Found multiple bearer tokens in the request",
-						"https://tools.ietf.org/html/rfc6750#section-3.1");
-				throw new OAuth2AuthenticationException(error);
-			}
-			return authorizationHeaderToken;
-		}
-		else if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
-			return parameterToken;
-		}
-		return null;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String resolve(HttpServletRequest request) {
+    String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
+    String parameterToken = resolveFromRequestParameters(request);
+    if (authorizationHeaderToken != null) {
+      if (parameterToken != null) {
+        BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
+                                                      HttpStatus.BAD_REQUEST,
+                                                      "Found multiple bearer tokens in the request",
+                                                      "https://tools.ietf.org/html/rfc6750#section-3.1");
+        throw new OAuth2AuthenticationException(error);
+      }
+      return authorizationHeaderToken;
+    } else if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
+      return parameterToken;
+    }
+    return null;
+  }
 
-	/**
-	 * Set if transport of access token using form-encoded body parameter is supported. Defaults to {@code false}.
-	 * @param allowFormEncodedBodyParameter if the form-encoded body parameter is supported
-	 */
-	public void setAllowFormEncodedBodyParameter(boolean allowFormEncodedBodyParameter) {
-		this.allowFormEncodedBodyParameter = allowFormEncodedBodyParameter;
-	}
+  /**
+   * Set if transport of access token using form-encoded body parameter is supported. Defaults to {@code false}.
+   *
+   * @param allowFormEncodedBodyParameter if the form-encoded body parameter is supported
+   */
+  public void setAllowFormEncodedBodyParameter(boolean allowFormEncodedBodyParameter) {
+    this.allowFormEncodedBodyParameter = allowFormEncodedBodyParameter;
+  }
 
-	/**
-	 * Set if transport of access token using URI query parameter is supported. Defaults to {@code false}.
-	 *
-	 * The spec recommends against using this mechanism for sending bearer tokens, and even goes as far as
-	 * stating that it was only included for completeness.
-	 *
-	 * @param allowUriQueryParameter if the URI query parameter is supported
-	 */
-	public void setAllowUriQueryParameter(boolean allowUriQueryParameter) {
-		this.allowUriQueryParameter = allowUriQueryParameter;
-	}
+  /**
+   * Set if transport of access token using URI query parameter is supported. Defaults to {@code false}.
+   *
+   * The spec recommends against using this mechanism for sending bearer tokens, and even goes as far as stating that it
+   * was only included for completeness.
+   *
+   * @param allowUriQueryParameter if the URI query parameter is supported
+   */
+  public void setAllowUriQueryParameter(boolean allowUriQueryParameter) {
+    this.allowUriQueryParameter = allowUriQueryParameter;
+  }
 
-	private static String resolveFromAuthorizationHeader(HttpServletRequest request) {
-		String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-		// APIGatewayCompatibleBearerTokenResolver divergence from DefaultBearerTokenResolver
-		// source class was also checking that the authorization string starts with "Bearer"
-		if (StringUtils.hasText(authorization)) {
-			Matcher matcher = authorizationPattern.matcher(authorization);
+  private static String resolveFromAuthorizationHeader(HttpServletRequest request) {
+    String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+    // APIGatewayCompatibleBearerTokenResolver divergence from DefaultBearerTokenResolver
+    // source class was also checking that the authorization string starts with "Bearer"
+    if (StringUtils.hasText(authorization)) {
+      Matcher matcher = authorizationPattern.matcher(authorization);
 
-			if (!matcher.matches()) {
-				BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_TOKEN,
-						HttpStatus.UNAUTHORIZED,
-						"Bearer token is malformed",
-						"https://tools.ietf.org/html/rfc6750#section-3.1");
-				throw new OAuth2AuthenticationException(error);
-			}
+      if (!matcher.matches()) {
+        BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_TOKEN,
+                                                      HttpStatus.UNAUTHORIZED,
+                                                      "Bearer token is malformed",
+                                                      "https://tools.ietf.org/html/rfc6750#section-3.1");
+        throw new OAuth2AuthenticationException(error);
+      }
 
-			return matcher.group("token");
-		}
-		return null;
-	}
+      return matcher.group("token");
+    }
+    return null;
+  }
 
-	private static String resolveFromRequestParameters(HttpServletRequest request) {
-		String[] values = request.getParameterValues("access_token");
-		if (values == null || values.length == 0)  {
-			return null;
-		}
+  private static String resolveFromRequestParameters(HttpServletRequest request) {
+    String[] values = request.getParameterValues("access_token");
+    if (values == null || values.length == 0) {
+      return null;
+    }
 
-		if (values.length == 1) {
-			return values[0];
-		}
+    if (values.length == 1) {
+      return values[0];
+    }
 
-		BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
-				HttpStatus.BAD_REQUEST,
-				"Found multiple bearer tokens in the request",
-				"https://tools.ietf.org/html/rfc6750#section-3.1");
-		throw new OAuth2AuthenticationException(error);
-	}
+    BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
+                                                  HttpStatus.BAD_REQUEST,
+                                                  "Found multiple bearer tokens in the request",
+                                                  "https://tools.ietf.org/html/rfc6750#section-3.1");
+    throw new OAuth2AuthenticationException(error);
+  }
 
-	private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
-		return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
-				|| (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
-	}
+  private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
+    return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
+            || (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
+  }
 }
--- oe-hr-portal-service-webapp/src/main/java/com/openenglish/hr/SecuredByClaimDataAccessInstrumentation.java
+++ oe-hr-portal-service-webapp/src/main/java/com/openenglish/hr/SecuredByClaimDataAccessInstrumentation.java
@@ -1,16 +1,7 @@
 package com.openenglish.hr;
 
 import com.openenglish.hr.service.JwtTokenService;
-import graphql.ExecutionResult;
-import graphql.execution.instrumentation.InstrumentationContext;
-import graphql.execution.instrumentation.InstrumentationState;
-import graphql.execution.instrumentation.SimpleInstrumentation;
-import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
-import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
-import graphql.schema.GraphQLArgument;
-import lombok.RequiredArgsConstructor;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -24,125 +15,138 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.InstrumentationState;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.GraphQLArgument;
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class SecuredByClaimDataAccessInstrumentation extends SimpleInstrumentation {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-    private static final String SECURED_BY_CLAIM_DIRECTIVE_NAME = "SecuredByClaim";
-    private static final String SECURED_BY_CLAIM_ARGUMENT_NAME = "claim";
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private static final String SECURED_BY_CLAIM_DIRECTIVE_NAME = "SecuredByClaim";
+  private static final String SECURED_BY_CLAIM_ARGUMENT_NAME = "claim";
 
-    @Value("${hrportal.purchaserIdSecurityCheck.enabled:true}")
-    private boolean purchaserIdSecurityCheck = true;
+  @Value("${hrportal.purchaserIdSecurityCheck.enabled:true}")
+  private boolean purchaserIdSecurityCheck = true;
 
-    private final JwtTokenService jwtTokenService;
+  private final JwtTokenService jwtTokenService;
 
-    private static final class DataAccessState implements InstrumentationState {
-        private Map<String, String> userAttributes;
+  private static final class DataAccessState implements InstrumentationState {
+
+    private Map<String, String> userAttributes;
+  }
+
+  @Override
+  public InstrumentationState createState() {
+    return new DataAccessState();
+  }
+
+  @Override
+  public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+    DataAccessState dataAccessState = parameters.getInstrumentationState();
+
+    if (purchaserIdSecurityCheck) {
+      JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken)
+          (SecurityContextHolder.getContext().getAuthentication());
+      String accessToken = jwtAuthenticationToken.getToken().getTokenValue();
+
+      // stores the user attributes in cognito,
+      // later used in all DataFetchers executed for the current GraphQL request
+      // to check data assertions
+      dataAccessState.userAttributes = jwtTokenService.getUserAttributes(accessToken);
     }
 
-    @Override
-    public InstrumentationState createState() {
-        return new DataAccessState();
+    return super.beginExecution(parameters);
+  }
+
+  private List<Pair<String, String>> extractClaimCheckBindings(InstrumentationFieldFetchParameters parameters) {
+    return parameters.getField().getArguments().stream()
+        .filter(arg -> arg.getDirective(SECURED_BY_CLAIM_DIRECTIVE_NAME) != null)
+        .map(this::toArgClaimPair)
+        .collect(Collectors.toList());
+  }
+
+  private Pair<String, String> toArgClaimPair(GraphQLArgument arg) {
+    String argumentName = arg.getName();
+    String claimName = (String) arg.getDirective(SECURED_BY_CLAIM_DIRECTIVE_NAME)
+        .getArgument(SECURED_BY_CLAIM_ARGUMENT_NAME).getValue();
+
+    return Pair.of(argumentName, claimName);
+  }
+
+  @Override
+  public DataFetcher<?> instrumentDataFetcher(DataFetcher<?> dataFetcher,
+                                              InstrumentationFieldFetchParameters parameters) {
+    // Only if security layer is enabled and only on user code
+    if (!purchaserIdSecurityCheck || parameters.isTrivialDataFetcher()) {
+      return dataFetcher;
     }
 
-    @Override
-    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
-        DataAccessState dataAccessState = parameters.getInstrumentationState();
+    List<Pair<String, String>> argNameClaimNamePairs = extractClaimCheckBindings(parameters);
+    DataAccessState state = parameters.getInstrumentationState();
 
-        if(purchaserIdSecurityCheck) {
-            JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken)
-                    (SecurityContextHolder.getContext().getAuthentication());
-            String accessToken = jwtAuthenticationToken.getToken().getTokenValue();
+    return environment -> {
+      if (!argNameClaimNamePairs.isEmpty()) {
+        verifyDataAccessAssertions(argNameClaimNamePairs, state, environment);
+      }
 
-            // stores the user attributes in cognito,
-            // later used in all DataFetchers executed for the current GraphQL request
-            // to check data assertions
-            dataAccessState.userAttributes = jwtTokenService.getUserAttributes(accessToken);
-        }
+      return dataFetcher.get(environment);
+    };
+  }
 
-        return super.beginExecution(parameters);
+  private void verifyDataAccessAssertions(
+      List<Pair<String, String>> argNameClaimNamePairs,
+      DataAccessState state,
+      DataFetchingEnvironment environment
+  ) {
+    for (Pair<String, String> argNameClaimNamePair : argNameClaimNamePairs) {
+      // A pair represents a data access security assertion, there can be 0..n assertions, all must pass
+      // to execute the dataFetcher. Required empty claims are treated as errors
+      //
+      // Given a graphql definition :
+      //   getPersons(salesforcePurchaserId: String! @SecuredByClaim(claim: "custom:purchaserId")): [Person]!
+      // the Pair under test here would be:
+      // argumentName = salesforcePurchaserId
+      // claimName = custom:purchaserId
+      // Using the names then the values are computed and compared
+      // - argumentValue from the GraphQL environment model
+      // - claimValue from the userAttributes retrieved from cognito
+      //
+      // Claim name in SecuredByClaim defaults to "custom:purchaserId" and is usually omitted.
+      // But it can be another field in the userAttributes, as an example:
+      // sendContactUsMessage(
+      //      salesforcePurchaserId: String! @SecuredByClaim,
+      //      name: String!,
+      //      email: String! @SecuredByClaim(claim: "email"),
+      //      message: String!
+      // ): MutationResult!
+      // would check both salesforcePurchaserId against custom:purchaserId and query param email against
+      // user attributes email
+
+      String argumentName = argNameClaimNamePair.getLeft();
+      String claimName = argNameClaimNamePair.getRight();
+
+      String claimValue = state.userAttributes.get(claimName);
+
+      if (StringUtils.isEmpty(claimValue)) {
+        throw new IllegalArgumentException("Missing required claim :" + claimName);
+      }
+
+      String argumentValue = environment.getArgument(argumentName);
+
+      if (!StringUtils.equals(argumentValue, claimValue)) {
+        logger.error("Claim vs Argument mismatch: query={} token={}", argumentValue, claimValue);
+        throw new IllegalArgumentException("Argument vs Claim mismatch: query="
+                                           + argumentValue + " token=" + claimValue);
+      }
     }
-
-    private List<Pair<String, String>> extractClaimCheckBindings(InstrumentationFieldFetchParameters parameters) {
-        return parameters.getField().getArguments().stream()
-                .filter(arg -> arg.getDirective(SECURED_BY_CLAIM_DIRECTIVE_NAME) != null)
-                .map(this::toArgClaimPair)
-                .collect(Collectors.toList());
-    }
-
-    private Pair<String, String> toArgClaimPair(GraphQLArgument arg) {
-        String argumentName = arg.getName();
-        String claimName = (String) arg.getDirective(SECURED_BY_CLAIM_DIRECTIVE_NAME)
-                .getArgument(SECURED_BY_CLAIM_ARGUMENT_NAME).getValue();
-
-        return Pair.of(argumentName, claimName);
-    }
-
-    @Override
-    public DataFetcher<?> instrumentDataFetcher(DataFetcher<?> dataFetcher, InstrumentationFieldFetchParameters parameters) {
-        // Only if security layer is enabled and only on user code
-        if (!purchaserIdSecurityCheck || parameters.isTrivialDataFetcher()) {
-            return dataFetcher;
-        }
-
-        List<Pair<String, String>> argNameClaimNamePairs = extractClaimCheckBindings(parameters);
-        DataAccessState state = parameters.getInstrumentationState();
-
-        return environment -> {
-            if (!argNameClaimNamePairs.isEmpty()) {
-                verifyDataAccessAssertions(argNameClaimNamePairs, state, environment);
-            }
-
-            return dataFetcher.get(environment);
-        };
-    }
-
-    private void verifyDataAccessAssertions(
-            List<Pair<String, String>> argNameClaimNamePairs,
-            DataAccessState state,
-            DataFetchingEnvironment environment
-    ) {
-        for (Pair<String, String> argNameClaimNamePair : argNameClaimNamePairs) {
-            // A pair represents a data access security assertion, there can be 0..n assertions, all must pass
-            // to execute the dataFetcher. Required empty claims are treated as errors
-            //
-            // Given a graphql definition :
-            //   getPersons(salesforcePurchaserId: String! @SecuredByClaim(claim: "custom:purchaserId")): [Person]!
-            // the Pair under test here would be:
-            // argumentName = salesforcePurchaserId
-            // claimName = custom:purchaserId
-            // Using the names then the values are computed and compared
-            // - argumentValue from the GraphQL environment model
-            // - claimValue from the userAttributes retrieved from cognito
-            //
-            // Claim name in SecuredByClaim defaults to "custom:purchaserId" and is usually omitted.
-            // But it can be another field in the userAttributes, as an example:
-            // sendContactUsMessage(
-            //      salesforcePurchaserId: String! @SecuredByClaim,
-            //      name: String!,
-            //      email: String! @SecuredByClaim(claim: "email"),
-            //      message: String!
-            // ): MutationResult!
-            // would check both salesforcePurchaserId against custom:purchaserId and query param email against
-            // user attributes email
-
-            String argumentName = argNameClaimNamePair.getLeft();
-            String claimName = argNameClaimNamePair.getRight();
-
-            String claimValue = state.userAttributes.get(claimName);
-
-            if (StringUtils.isEmpty(claimValue)) {
-                throw new IllegalArgumentException("Missing required claim :" + claimName);
-            }
-
-            String argumentValue = environment.getArgument(argumentName);
-
-            if (!StringUtils.equals(argumentValue, claimValue)) {
-                logger.error("Claim vs Argument mismatch: query={} token={}", argumentValue, claimValue);
-                throw new IllegalArgumentException("Argument vs Claim mismatch: query="
-                        + argumentValue + " token=" + claimValue);
-            }
-        }
-    }
+  }
 }
